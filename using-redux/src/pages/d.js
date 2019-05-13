db4f9d6fb52e9d0df8f711da190d66adc3f8d5c5
Toma Morisawa <13.35.104@gmail.com>
2019-05-13 23:47:14 +0900

    add using-redux files

---
+++ using-redux/src/pages/d.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const D = () => (
+  <div>
+    <li>
+    <Link to="/b/">b</Link>
+    </li>
+    <li>
+    <Link to="/c/">c</Link>
+    </li>
+    <li>
+    <Link to="/d/">d</Link>
+    </li>
+    <p>d</p>
+  </div>
+)
+
+export default D
